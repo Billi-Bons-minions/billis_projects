881f1d593e19113221622d13e82905795e11e6ad
Billi-Bons-minions <minions.duke.minions@gmail.com>
2024-05-24 10:27:00 +0200

    Add files via upload

--- Kalender.py
+++ Kalender.py
@@ -1,9 +1,26 @@
+#!/usr/bin/env python3
 import tkinter as tk
 from tkinter import messagebox
-from tkcalendar import Calendar,DateEntry
+from tkcalendar import Calendar
+from tkinter.scrolledtext import ScrolledText
 import sqlite3
-
 def show(event):
+    def bearbeitet_speichern():
+        conn=sqlite3.connect("entrys.db")
+        selected_index = liste.curselection()
+        selected_event = liste.get(selected_index)
+        selected_event = selected_event.replace("Datum: ", "")
+        selected_event = selected_event.replace("Ereignis: ", "")
+        selected_date, selected_title = selected_event.split(", ")
+        beschreibung_bearbeited=(show_description.get("1.0","end"))
+        conn.execute("DELETE FROM entrys WHERE Datum=? AND Überschrift=?", (selected_date, selected_title))
+        conn.commit()
+        conn.execute("INSERT INTO entrys (Datum, Überschrift, Beschreibung) VALUES (?,?,?)",(selected_date, selected_title, beschreibung_bearbeited))
+        conn.commit()
+        conn.close()
+        messagebox.showinfo(title="Eintrag",message="Eintrag erfolgreich bearbeitet")
+        show_event.destroy()
+        load()
     def delete():
         conn = sqlite3.connect("entrys.db")
         selected_index = liste.curselection()
@@ -16,6 +33,7 @@ def show(event):
         conn.close()
         load()
         show_event.destroy()
+        messagebox.showinfo(title="Eintrag", message="Eintrag erfolgreich gelöscht")
     conn = sqlite3.connect("entrys.db")
     cursor = conn.cursor()
     selected_index = liste.curselection()
@@ -32,15 +50,17 @@ def show(event):
     show_event.title("Ereignis")
     show_event.geometry(f"{screen_x//4}x{screen_y//4}")
     show_y=screen_y//4
-    show_y=show_y*0.035
+    show_y=show_y*0.027
     show_event.resizable(False,False)
     show_date=tk.Label(show_event,text=date).pack()
     show_title=tk.Label(show_event,text=title).pack()
-    show_description=(tk.Text(show_event,width=(screen_x//4),height=show_y))
+    show_description=ScrolledText(show_event,width=(screen_x//4),height=show_y)
     show_description.pack()
     show_description.insert('1.0', description)
-    show_description['state'] = 'disabled'
-    delete_button=tk.Button(show_event,text="Löschen",command=delete).pack()
+    #show_description['state'] = 'disabled'
+    speichern_button=tk.Button(show_event,text="Speichern",foreground="green",command=bearbeitet_speichern)
+    speichern_button.pack()
+    delete_button=tk.Button(show_event,text="Löschen",command=delete,foreground="red").pack()
     show_event.mainloop()
 
 def load():
@@ -56,13 +76,21 @@ def add_entry(event):
             messagebox.showerror("Fehler","Keine gültige Überschrift")
             eintrag.destroy()
             return
-
-
-        Beschreibung = beschreibung_eingabe.get("1.0", "end")
-        verb.execute("INSERT INTO entrys (Datum, Überschrift, Beschreibung) VALUES (?,?,?)",(selected_date, Ueberschrift, Beschreibung))
-        verb.commit()
-        load()
-        eintrag.destroy()
+        conn = sqlite3.connect("entrys.db")
+        cursor = conn.cursor()
+        cursor.execute("SELECT Datum, Überschrift FROM entrys WHERE Datum=? AND Überschrift=?",(selected_date, Ueberschrift))
+        selected_event = cursor.fetchone()
+        conn.close()
+        if selected_event is None:
+            Beschreibung = beschreibung_eingabe.get("1.0", "end")
+            verb.execute("INSERT INTO entrys (Datum, Überschrift, Beschreibung) VALUES (?,?,?)",(selected_date, Ueberschrift, Beschreibung))
+            verb.commit()
+            load()
+            eintrag.destroy()
+            messagebox.showinfo(title="Eintrag",message="Eintrag erfolgreich gespeichert")
+        else:
+            eintrag.destroy()
+            messagebox.showerror(title="Eintrag",message="Eintrag schon vorhanden")
     eintrag = tk.Tk()
     eintrag.title("Eintrag")
     eintrag.geometry(f"{screen_x//4}x{screen_y//4}")
@@ -75,9 +103,9 @@ def add_entry(event):
     überschrift_eingabe.pack(fill="x")
     beschreibung = tk.Label(eintrag,text="Beschreibung:")
     beschreibung.pack()
-    beschreibung_eingabe = tk.Text(eintrag,width=(screen_x//4),height=show_y)
+    beschreibung_eingabe = ScrolledText(eintrag,width=(screen_x//4),height=show_y)
     beschreibung_eingabe.pack()
-    eingabe = tk.Button(eintrag,text="Speichern",command=lambda:beenden(überschrift_eingabe.get(),beschreibung_eingabe))
+    eingabe = tk.Button(eintrag,text="Speichern",foreground="green",command=lambda:beenden(überschrift_eingabe.get(),beschreibung_eingabe))
     eingabe.pack()
     #eintrag.mainloop()
 
